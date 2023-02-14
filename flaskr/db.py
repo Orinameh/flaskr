d5d76d52fdf9f2ff012e5bcfa1f7e23c5e88d00b
Orinameh <memefacto@yahoo.com>
2023-02-14 21:32:55 +0100

    python flask tutorial

---
+++ flaskr/db.py
@@ -0,0 +1,41 @@
+import sqlite3
+
+import click
+# g is a special object that is unique for each request. It is used to store data that might be accessed by multiple functions during the request
+from flask import current_app, g
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        # sqlite3.Row tells the connection to return rows that behave like dicts. 
+        g.db.row_factory = sqlite3.Row
+    
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+        
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf-8'))
+
+@click.command('init-db')
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initializing database')
+
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
